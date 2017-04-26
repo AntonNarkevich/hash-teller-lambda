860871e174ee4d747d8d00df4a9446470802624d
antonio.narkevich <antonio.narkevich@gmail.com>
2017-04-26 11:57:05 +0300

    Does not call rekognition anymore. Also reformats the code.

--- handler.js
+++ handler.js
@@ -3,40 +3,21 @@
 const path = require('path');
 const co = require('co');
 const Promise = require('bluebird');
-const _ = require('lodash');
 const AWS = require('aws-sdk');
 const execFileAsync = Promise.promisify(require('child_process').execFile);
 
-AWS.config.update({region: process.env['REGION']});
+AWS.config.update({ region: process.env['REGION'] });
+
 //Setting the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+process.env['PATH'] =
+	process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 module.exports.generateTags = (event, context, callback) => {
-	co(function *() {
-		let rekognition = Promise.promisifyAll(new AWS.Rekognition());
-
-		let labelsData = yield rekognition.detectLabelsAsync({
-			Image: {
-				S3Object: {
-					Bucket: 'instagram-hastag-generator',
-					Name: event.s3Key
-				}
-			}
-		});
-
-		//Take first five labels joing them like this: 'horse force power'
-		let labels = _(labelsData.Labels)
-			.take(5)
-			.map(d => d.Name)
-			.join(' ');
-
+	co(function*() {
 		//Phantom binary
 		let phantomPath = path.join(__dirname, 'phantomjs-amazon-linux-executable');
 		//Arguments for the phantom script
-		let processArgs = [
-			path.join(__dirname, 'phantom-script.js'),
-			labels
-		];
+		let processArgs = [path.join(__dirname, 'phantom-script.js'), event.labels];
 
 		//Run the process return stdout
 		let tagsString = yield execFileAsync(phantomPath, processArgs);
--- phantom-script.js
+++ phantom-script.js
@@ -3,29 +3,40 @@
 var system = require('system');
 var webPage = require('webpage');
 
-
 //Expecting the labels as command line arguments
 if (system.args.length < 2) {
-	throw new Error('Labels are not passed')
+	throw new Error('Labels are not passed');
 }
 var labels = system.args.slice(1, system.args.length).join(' ');
-
 
 var page = webPage.create();
 
 //Set up error handlers to see if phantom fails
-page.onResourceError = function (resourceError) {
+page.onResourceError = function(resourceError) {
 	system.stderr.writeLine('= onResourceError()');
-	system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
-	system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString);
+	system.stderr.writeLine(
+		'  - unable to load url: "' + resourceError.url + '"'
+	);
+	system.stderr.writeLine(
+		'  - error code: ' +
+			resourceError.errorCode +
+			', description: ' +
+			resourceError.errorString
+	);
 };
-page.onError = function (msg, trace) {
+page.onError = function(msg, trace) {
 	system.stderr.writeLine('= onError()');
 	var msgStack = ['  ERROR: ' + msg];
 	if (trace) {
 		msgStack.push('  TRACE:');
-		trace.forEach(function (t) {
-			msgStack.push('    -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+		trace.forEach(function(t) {
+			msgStack.push(
+				'    -> ' +
+					t.file +
+					': ' +
+					t.line +
+					(t.function ? ' (in function "' + t.function + '")' : '')
+			);
 		});
 	}
 	system.stderr.writeLine(msgStack.join('\n'));
@@ -33,7 +44,7 @@ page.onError = function (msg, trace) {
 
 page.settings.loadImages = false;
 
-page.open('https://displaypurposes.com/', function (status) {
+page.open('https://displaypurposes.com/', function(status) {
 	if (status !== 'success') {
 		phantom.exit();
 
@@ -41,22 +52,22 @@ page.open('https://displaypurposes.com/', function (status) {
 	}
 
 	//Put the labels into the input field
-	page.evaluate(function (labels) {
+	page.evaluate(function(labels) {
 		var input = document.querySelector('.search-component input');
 		input.value = labels;
 		//Have to trigger input event to trigger processing
-		input.dispatchEvent(new Event('input', {'bubbles': true}));
+		input.dispatchEvent(new Event('input', { bubbles: true }));
 	}, labels);
 
 	//Wait and then copy out the tags
-	setTimeout(function () {
-		var tags = page.evaluate(function () {
+	setTimeout(function() {
+		var tags = page.evaluate(function() {
 			//Check if at least one tag was generated
 			if (!document.querySelector('.copyable-component > div .tag-pack')) {
 				return;
 			}
 
-			var deepText = function (node) {
+			var deepText = function(node) {
 				var A = [];
 				if (node) {
 					node = node.firstChild;
